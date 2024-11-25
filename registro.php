6c9d8118fb9bfd4a22f02c31e9d36a5ff8cc6ceb
Diego-Ulpin12 <150453673+DylantriX@users.noreply.github.com>
2024-11-25 12:47:20 -0300

    Update registro.php

--- registro.php
+++ registro.php
@@ -3,10 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro</title> href= "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-
+    <title>Registro</title> 
+    <link rel="styleesheet" href= "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 </head>
 <body>
-    
+    <div class = "container mt-5">
+        <h2>Registro de Usuario</h2>
+        
 </body>
 </html>
